e240a8a3d03f5e9e7a4fc191d7e5790f8d648d43
AbayoHJovin <abayohirwajovin@gmail.com>
2025-09-16 15:19:50 +0200

    improved the url to be used

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,5 @@
 // API Configuration
-export const API_BASE_URL =
-  // process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/api/v1";
-  "http://44.201.73.159:8081/api/v1";
+export const API_BASE_URL = "http://44.201.73.159:8081/api/v1";
 
 // API Endpoints
 export const API_ENDPOINTS = {
